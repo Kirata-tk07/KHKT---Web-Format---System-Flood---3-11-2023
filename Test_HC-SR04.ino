4816ffe33ee9a8aa4159e630d9a3ddb7fe192825
Kirata-tk07 <122148690+Kirata-tk07@users.noreply.github.com>
2023-11-04 20:26:37 +0700

    Update Test_HC-SR04.ino

--- Test_HC-SR04.ino
+++ Test_HC-SR04.ino
@@ -1,39 +1,83 @@
-int trigPin = 11;    // Trigger
-int echoPin = 12;    // Echo
-long duration, cm, inches;
- 
+#include <Arduino.h>
+
+// Định nghĩa các trig và echo
+const int TRIG_PIN = 8;
+const int ECHO_PIN = 7;
+
 void setup() {
-  //Serial Port begin
-  Serial.begin (9600);
-  //Define inputs and outputs
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
 }
- 
-void loop() {
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(5);
-  digitalWrite(trigPin, HIGH);
+
+// Định nghĩa hàm để đo khoảng cách đến một vật thể
+int measure_distance() {
+  // Gửi xung đến chân kích hoạt
+  digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
- 
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
- 
-  // Convert the time into a distance
-  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
-  inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
-  
-  Serial.print(inches);
-  Serial.print("in, ");
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
-  
-  delay(250);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // Chờ xung hồi âm
+  unsigned long start_time = micros();
+  while (!digitalRead(ECHO_PIN)) {
+  }
+  unsigned long end_time = micros();
+
+  // Tính toán khoảng cách
+  return pulseIn(ECHO_PIN, HIGH) * 0.0343 / 2;
+}
+
+// Định nghĩa hàm để lấy nhiều lần đo và lấy trung bình của chúng
+int take_average_measurement(int num_samples) {
+  int distances[num_samples];
+  for (int i = 0; i < num_samples; i++) {
+    distances[i] = measure_distance();
+  }
+
+  int average = 0;
+  for (int i = 0; i < num_samples; i++) {
+    average += distances[i];
+  }
+
+  return average / num_samples;
+}
+
+// Chỉnh lại cảm biến bằng một khoảng cách đã biết
+void calibrate_sensor(int known_distance) {
+  int measured_distance = take_average_measurement(10);
+
+  float calibration_factor = known_distance / measured_distance;
+
+  Serial.print("Calibration factor: ");
+  Serial.println(calibration_factor);
+}
+
+// Định nghĩa hàm bù nhiệt độ
+int temperature_compensate(int distance, float temperature) {
+  float speed_of_sound = 331.3 + 0.6 * temperature * (1 + 0.00367 * temperature);
+
+  int compensated_distance = distance * 331.3 / speed_of_sound;
+
+  return compensated_distance;
+}
+
+// Bắt đầu vòng lặp đo
+void loop() {
+  // Chỉnh lại cảm biến
+  calibrate_sensor(30);
+
+  // Bắt đầu vòng lặp đo
+  while (true) {
+    // Lấy một lần đo trung bình
+    int measured_distance = take_average_measurement(10);
+
+    // Bù nhiệt độ cho phép đo
+    float temperature = 25;
+    int compensated_distance = temperature_compensate(measured_distance, temperature);
+
+    // In phép đo đã bù
+    Serial.print("Distance: ");
+    Serial.println(compensated_distance);
+
+    // Chờ 1 giây
+    delay(1000);
+  }
 }
